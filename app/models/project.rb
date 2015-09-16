7b0f1b9f577d0b3cb2909ada8a5156abc058c61a
Michael Tomko <tomko.michael@gmail.com>
2015-09-16 19:36:26 +0000

    Fixed up basic framework

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :tasks
   validates :name, :description, presence: true
-  validates :description, length: { maxiumum: 140 }
+  validates :description, length: { maximum: 140 }
   validates :name, length: { maximum: 40 }
 end
