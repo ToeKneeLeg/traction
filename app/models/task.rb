65bf5cc7bf3f602e9cff8b870441daf55c27f7d2
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-17 19:16:49 -0400

    Built task assignment, deleted psuedo code notes, made seed db

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,27 +6,15 @@ class Task < ActiveRecord::Base
   validates :description, :required_skill, presence: true
   validates :description, length: { maximum: 200 }
 
-  after_create do 
-    puts "La De Da"
-    # member = Member.all
-    # member.find do |member|
-    #   if member.tasks.empty? && member.skills.exists?(self.required_skill)
-    #     self.member_id = member.id 
-    #   end
-    # end
-  end
-
-  # protected
-
-  # def assign_task
-  #   self.description = "this worked"
-  #  #  puts "La De Da"
-  # 	# member = Member.all
-  # 	# member.find do |member|
-  # 	# 	if member.tasks.empty? && member.skills.exists?(self.required_skill)
-  # 	# 		self.member_id = member.id 
-  #  #    end
-  # 	# end
-  # end	
+  before_create :assign_task
+  
+  def assign_task
+  	member = Member.all
+  	member.find do |member|
+  		if member.tasks.empty? && member.skills.exists?(self.required_skill)
+  			self.member_id = member.id 
+      end 
+  	end
+  end	
 
 end
